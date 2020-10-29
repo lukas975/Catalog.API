7a76405d5ae19fe9878495a6c4f83f777baad6c3
Luka <sabo.luka@gmail.com>
2020-10-29 13:52:44 +0100

    API, Domain, Infrastructure

---
+++ src/Catalog.Domain/Repositories/IItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain.Repositories
+{
+    public interface IItemRepository
+    {
+        Task<IEnumerable<Item>> GetAsync();
+        Task<Item> GetAsync(Guid id);
+        Item Add(Item item);
+        Item Update(Item item);
+    }
+}
---
+++ src/Catalog.Infrastructure/CatalogContext.cs
@@ -0,0 +1,35 @@
+﻿using Catalog.Domain.Entities;
+using Catalog.Domain.Repositories;
+using Catalog.Infrastructure.SchemaDefinitions;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure
+{
+    public class CatalogContext : DbContext, IUnitOfWork
+    {
+        public const string DEFAULT_SCHEMA = "catalog";
+        public DbSet<Item> Items { get; set; }
+
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ItemEntitySchemaDefinition());
+            modelBuilder.ApplyConfiguration(new GenreEntitySchemaConfiguration());
+            modelBuilder.ApplyConfiguration(new ArtistEntitySchemaConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            await SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
