23ae2f3dcc02a3994600585572d334983b9a2273
Luka <sabo.luka@gmail.com>
2020-11-02 14:43:58 +0100

    artist, genre controler

---
+++ src/Catalog.Domain/Requests/Artist/Validators/AddArtistRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Catalog.Domain.Requests.Artist.Validators
+{
+    public class AddArtistRequestValidator : AbstractValidator<AddArtistRequest>
+    {
+        public AddArtistRequestValidator()
+        {
+            RuleFor(artist => artist.ArtistName).NotEmpty();
+        }
+    }
+}
---
+++ src/Catalog.Domain/Requests/Genre/Validators/AddGenreRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Catalog.Domain.Requests.Genre.Validators
+{
+    public class AddGenreRequestValidator : AbstractValidator<AddGenreRequest>
+    {
+        public AddGenreRequestValidator()
+        {
+            RuleFor(genre => genre.GenreDescription).NotEmpty();
+        }
+    }
+}
