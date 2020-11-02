e88910ff459301fba3aa4f46785ca7bc0d139ca8
Luka <sabo.luka@gmail.com>
2020-11-02 14:15:48 +0100

    validators

--- src/Catalog.Domain/Requests/Item/Validators/EditItemRequestValidator.cs
+++ src/Catalog.Domain/Requests/Item/Validators/EditItemRequestValidator.cs
@@ -1,18 +1,67 @@
-﻿using FluentValidation;
+﻿using Catalog.Domain.Requests.Artist;
+using Catalog.Domain.Requests.Genre;
+using Catalog.Domain.Services;
+using FluentValidation;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Catalog.Domain.Requests.Item.Validators
 {
     public class EditItemRequestValidator : AbstractValidator<EditItemRequest>
     {
-        public EditItemRequestValidator()
+        private readonly IArtistService _artistService;
+        private readonly IGenreService _genreService;
+
+        public EditItemRequestValidator(IArtistService artistService, IGenreService genreService)
         {
-            RuleFor(x => x.Id).NotEmpty();
-            RuleFor(x => x.GenreId).NotEmpty();
-            RuleFor(x => x.ArtistId).NotEmpty();
-            RuleFor(x => x.Price).NotEmpty();
-            RuleFor(x => x.Price).Must(x => x?.Amount > 0);
-            RuleFor(x => x.ReleaseDate).NotEmpty();
-            RuleFor(x => x.Name).NotEmpty();
+            _genreService = genreService;
+            _artistService = artistService;
+
+            RuleFor(x => x.Id)
+                .NotEmpty();
+
+            RuleFor(x => x.GenreId)
+                .NotEmpty()
+                .MustAsync(GenreExists).WithMessage("Genre must exists");
+
+            RuleFor(x => x.ArtistId)
+                .NotEmpty()
+                .MustAsync(ArtistExists).WithMessage("Artist must exists");
+
+            RuleFor(x => x.Price)
+                .NotEmpty();
+            
+            RuleFor(x => x.Price)
+                .Must(x => x?.Amount > 0);
+            
+            RuleFor(x => x.ReleaseDate)
+                .NotEmpty();
+            
+            RuleFor(x => x.Name)
+                .NotEmpty();
+        }
+
+        private async Task<bool> ArtistExists(Guid artistId, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(artistId.ToString()))
+            {
+                return false;
+            }
+
+            var artist = await _artistService.GetArtistAsync(new GetArtistRequest { Id = artistId });
+            return artist != null;
+        }
+
+        private async Task<bool> GenreExists(Guid genreId, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(genreId.ToString()))
+            {
+                return false;
+            }
+
+            var genre = await _genreService.GetGenreAsync(new GetGenreRequest { Id = genreId });
+            return genre != null;
         }
     }
 }
